4a1c7e978395174bc997a5f0298d7d482c272d82
Roma-Leto <imletoroma@gmail.com>
2023-03-02 15:13:00 +0300

    add user model

---
+++ blog/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class BUser(AbstractUser):
+    is_activated = models.BooleanField(default=True, db_index=True, verbose_name='Прошёл активацию?')
+
+
+    class Meta(AbstractUser):
+        pass
