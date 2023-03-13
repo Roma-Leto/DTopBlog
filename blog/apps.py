8a173615de750dcb58615d247f8d581553155ac3
Roma-Leto <imletoroma@gmail.com>
2023-03-13 18:07:47 +0300

    add utilities

--- blog/apps.py
+++ blog/apps.py
@@ -1,5 +1,17 @@
 from django.apps import AppConfig
 
+from django.dispatch import Signal
+
+
+user_registered = Signal('instance')
+
+
+def user_registered_dispatcher(sender, **kwargs):
+    send_activation_notification(kwargs['instance'])
+
+
+user_registered.connect(user_registered_dispatcher)
+
 
 class BlogConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
---
+++ blog/utilities.py
@@ -0,0 +1,7 @@
+from django.template.loader import render_to_string
+from django.core.singing import Signer
+
+from dtblog.settings import ALLOWED_HOSTS
+
+
+
